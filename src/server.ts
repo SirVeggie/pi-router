cbb9b201c3a02ad404aea1919f57a409968dd598
SirVeggie <32365239+SirVeggie@users.noreply.github.com>
2024-02-11 14:59:50 +0200

    add health check

--- src/server.ts
+++ src/server.ts
@@ -6,37 +6,41 @@ import { wakeDevice } from './wake';
 
 export function createServer() {
     const app = express();
-    
+
     app.use(express.json());
     app.use(morgan('tiny'));
-    
+
     //====| routes |====//
-    
+
+    app.get('/health', (req, res) => {
+        res.status(200).json({ status: 'ok' });
+    });
+
     app.get('/api/wake/pc', (req, res) => {
         wakeDevice(config.PC_MAC);
         res.status(200).end();
     });
-    
+
     app.get('/api/wake/tv', (req, res) => {
         wakeDevice(config.TV_MAC);
         res.status(200).end();
     });
-    
+
     //====| middleware |====//
-    
+
     function unknownEndpoint(req: any, res: any) {
         res.status(404).json({ error: 'unknown endpoint' });
     }
-    
+
     function errorHandler(error: any, req: any, res: any, next: any) {
         console.error('Error: ' + error);
-        
+
         next(error);
     }
-    
+
     app.use(unknownEndpoint);
     app.use(errorHandler);
-    
+
     return app.listen(config.PORT, () => {
         console.log(`Server running on port ${config.PORT}`);
     });
